5bbd527d8613945351044ce61cfbe20074f5a1ca
Jon Bullock <jondooger@gmail.com>
2017-03-14 16:42:35 -0600

    Lint one more file.

--- src/3_social_network/server.js
+++ src/3_social_network/server.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import graphQLHTTP from 'express-graphql';
-import path from 'path';
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
-import {Schema} from './data/schema';
+import express from 'express'
+import graphQLHTTP from 'express-graphql'
+import path from 'path'
+import webpack from 'webpack'
+import WebpackDevServer from 'webpack-dev-server'
+import {Schema} from './data/schema'
 
-const APP_PORT = 3000;
-const GRAPHQL_PORT = 8080;
+const APP_PORT = 3000
+const GRAPHQL_PORT = 8080
 
 // Expose a GraphQL endpoint
-var graphQLServer = express();
+const graphQLServer = express()
 graphQLServer.use('/', graphQLHTTP({
   graphiql: true,
   pretty: true,
-  schema: Schema,
-}));
+  schema: Schema
+}))
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
-));
+))
 
 // Serve the Relay app
 const compiler = webpack({
@@ -27,20 +27,20 @@ const compiler = webpack({
       {
         exclude: /node_modules/,
         loader: 'babel',
-        test: /\.js$/,
+        test: /\.js$/
       }
     ]
   },
   output: {filename: 'app.js', path: '/'}
-});
+})
 const app = new WebpackDevServer(compiler, {
   contentBase: '/public/',
   proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
   publicPath: '/js/',
   stats: {colors: true}
-});
+})
 // Serve static resources
-app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.use('/', express.static(path.resolve(__dirname, 'public')))
 app.listen(APP_PORT, () => {
-  console.log(`App is now running on http://localhost:${APP_PORT}`);
-});
+  console.log(`App is now running on http://localhost:${APP_PORT}`)
+})
